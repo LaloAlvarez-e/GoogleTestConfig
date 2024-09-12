22dc68e1c10d197010a5cf759f12c95fd91117b1
Lalo Alvarez <laloalvarez.e@gmail.com>
2024-09-11 21:24:33 -0600

    Adding Fetch process and local process

---
+++ LocalFramework/Tests/sumTest.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include "sum.h"
+
+Error_nCode sum(int a, int b, int *result);
+// Test cases
+TEST(SumTest, PositiveNumbers) {
+    int result = 0;
+    Error_nCode enErrorCode = sum(5, 10, &result);
+
+    EXPECT_EQ(result, 15);
+}
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ LocalFramework/defines.h
@@ -0,0 +1,12 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+typedef enum
+{
+    Error_enCode_OK,
+    Error_enCode_ERROR,
+    Error_enCode_UNDEF,
+}Error_nCode;
+
+
+#endif // DEFINES_H
